7929a7a163468ae5b67fd74fed45a672769d1b2d
Aleksefo <aleksefo@gmail.com>
2020-05-10 12:42:28 +0300

    Cleanup code

--- constants/Config.js
+++ constants/Config.js
@@ -1,6 +1,6 @@
- const Config = {
-    redditURL: 'https://www.reddit.com/',
-    subRedditName: 'r/ClimateActionPlan/'
-}
+const Config = {
+  redditURL: "https://amp.reddit.com/",
+  subRedditName: "r/ClimateActionPlan/",
+};
 
- export default Config
+export default Config;
