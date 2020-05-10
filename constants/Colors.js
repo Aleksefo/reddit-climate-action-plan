8bb6011215e3fe6c2c05f8ea12e4e4ad5c8f2701
Aleksefo <aleksefo@gmail.com>
2020-05-10 12:13:31 +0300

    Add colors to Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,9 @@
-const tintColor = '#2f95dc';
-
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  background: "#e1f5e1",
+  gray100: "#e3e3e3",
+  gray200: "#c5c5c5",
+  gray500: "#838688",
+  white: "#fff",
+  black: "#000000",
+  primary: "#248a38",
 };
