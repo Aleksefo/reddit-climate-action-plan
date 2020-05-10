60d619e10a535d392fe1e08caac6e5409a366ca2
Aleksefo <aleksefo@gmail.com>
2020-05-10 10:34:43 +0300

    Add basic UI for posts list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,53 +1,119 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-import { MonoText } from '../components/StyledText';
+import * as WebBrowser from "expo-web-browser";
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import Config from "../constants/Config";
+import { FontAwesome5, AntDesign } from "@expo/vector-icons";
 
 export default function HomeScreen() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${Config.redditURL}${Config.subRedditName}new.json?sort=new`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data.data.children}
+          style={{ margin: 4 }}
+          keyExtractor={({ data }) => data.id}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              key={item.data.id}
+              onPress={() => handleLinkPress(item.data.permalink)}
+              style={{
+                borderWidth: 1,
+                borderRadius: 4,
+                borderColor: "#c5c5c5",
+                backgroundColor: "#f5f5f5",
+                margin: 4,
+                padding: 4,
+                height: 96,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  flex: 1,
+                  justifyContent: "space-between",
+                }}
+              >
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                  <Text
+                    style={styles.titleText}
+                    numberOfLines={3}
+                    ellipsizeMode="tail"
+                  >
+                    {item.data.link_flair_text && (
+                      <Text style={styles.flairText}>
+                        {` ${item.data.link_flair_text} `}
+                      </Text>
+                    )}
+                    {item.data.title}
+                  </Text>
+                </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+                <Image
+                  source={{ uri: item.data.thumbnail }}
+                  style={{ width: 50, height: 50 }}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                  <Text style={styles.infoText}>By {item.data.author}</Text>
+                  {item.data.author_flair_text && (
+                    <Text style={styles.flairText}>
+                      {item.data.author_flair_text}
+                    </Text>
+                  )}
+                </View>
+                <FontAwesome5
+                  name="hotjar"
+                  size={14}
+                  color="green"
+                  style={{ marginHorizontal: 4 }}
+                />
+                <Text style={styles.infoText}>{item.data.ups}</Text>
+                <AntDesign
+                  name="like1"
+                  size={14}
+                  color="green"
+                  style={{ marginHorizontal: 4 }}
+                />
+                <Text style={styles.infoText}>{item.data.num_comments}</Text>
+              </View>
+              {/*<Text>{item.data.created}</Text>*/}
+              {/*<Text>{item.data.created}</Text>*/}
+              {/*<Text>{item.data.permalink}</Text>*/}
+              {/*<Text style={styles.codeHighlightText}>{item.data.link_flair_text}</Text>*/}
+            </TouchableOpacity>
+          )}
+        />
+      )}
 
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
+      <View style={styles.tabBarInfoContainer} />
     </View>
   );
 }
@@ -56,124 +122,47 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
+function handleLinkPress(link) {
+  WebBrowser.openBrowserAsync(Config.redditURL + link);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+    backgroundColor: "#e1f5e1",
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: -5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
       },
       android: {
         elevation: 20,
       },
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 1,
   },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+  titleText: {
+    fontSize: 18,
+    // lineHeight: 22,
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
+  infoText: {
     fontSize: 14,
-    color: '#2e78b7',
+  },
+  flairText: {
+    backgroundColor: "#d2d2d2",
+    marginLeft: 4,
+    paddingHorizontal: 2,
+    fontSize: 14,
+    // lineHeight: 22,
   },
 });
